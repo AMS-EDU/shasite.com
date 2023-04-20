445e67fc3c37571caaba5eeb5a510bf619d575e6
AMS-EDU <129628495+AMS-EDU@users.noreply.github.com>
2023-04-20 18:21:44 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+$name = "User";
+echo "If you can read it, CONGRATULATION, ${name} <br>";
+echo "This is second line, successfull? <br>";
+echo "Is your favourite number 7 or ", 7-4,"? <br>";
+?>
