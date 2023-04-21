9f4a2802cf40cf22ea1e9d5a368d335ecc330d8b
AMS-EDU <ahammed.muhammadshafin_alunno@einaudi-giordano.edu.it>
2023-04-21 18:29:55 +0200

    is it working...
    I hope git is fixed :/

--- index.php
+++ index.php
@@ -3,4 +3,6 @@ $name = "User";
 echo "If you can read it, CONGRATULATION, ${name} <br>";
 echo "This is second line, successfull? <br>";
 echo "Is your favourite number 7 or ", 7-4,"? <br>";
+
+echo "ASSALAMUALAIKUM!</b>";
 ?>
