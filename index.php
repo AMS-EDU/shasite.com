cf64c6a2f6d1cc69dad60eaa59ffe129e4c2dedf
[AMS-EDU] <[ahammed.muhammadshafin_alunno@einaudi-giordano.edu.it]>
2023-04-23 00:31:05 +0200

    foobar

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ echo "If you can read it, CONGRATULATION, ${name} <br>";
 echo "This is second line, successfull? <br>";
 echo "Is your favourite number 7 or ", 7-4,"? <br>";
 
-echo "ASSALAMUALAIKUM!</b>";
+echo "HELLO THERE</b>";
 ?>
