795820d6fabbf0b39870ec5d6114e75c8e494f9c
AM-Shafin <ahammedshafin10901@gmail.com>
2023-04-25 22:17:24 +0200

    Basics of variable and variable syntex

---
+++ indexvar.php
@@ -0,0 +1,8 @@
+<?php
+
+ $string = "Foobar"; // php variable syntax: $<variablename> {case sensitive} 
+ $ID = "00149"; //variable types doesn't need to be defined durin declaration
+ echo "This string is '$string' with id #$ID.<br>";
+ echo "variable case-sensitivity test: $String. <br>"; //testing case sensitivity of vars
+
+?>
