0c91daedb1718e018401fba582fe24e7c2ede71b
AM-Shafin <ahammedshafin10901@gmail.com>
2023-04-25 22:38:25 +0200

    A bit more about variables, their types and uses

--- indexvar.php
+++ indexvar.php
@@ -1,8 +1,20 @@
 <?php
 
- $string = "Foobar"; // php variable syntax: $<variablename> {case sensitive} 
- $ID = "00149"; //variable types doesn't need to be defined durin declaration
- echo "This string is '$string' with id #$ID.<br>";
- echo "variable case-sensitivity test: $String. <br>"; //testing case sensitivity of vars
+    $string = "Foobar"; // php variable syntax: $<variablename> {case sensitive} 
+    $ID = "00149"; //variable types doesn't need to be defined durin declaration
+    echo "This string is '$string' with id #$ID.<br>";
+    echo "variable case-sensitivity test: $String. <br>"; //testing case sensitivity of vars
+
+// Boolean condition
+    if (TRUE)
+        print("This will always print <br>");
+    else
+        print("THis will never print<br>");
+
+// Doubles decimal place demo
+    $many = 2.2888889939;
+    $many_2 = 2.21239923;
+    $few = $many + $many_2;
+    print("$many + $many = $few <br>");
 
 ?>
